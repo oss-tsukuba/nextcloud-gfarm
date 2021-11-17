1bf22ddb5bccf0c0528a6214522cc929d1ee060a
Takuya Ishibashi <takuya-i@users.sourceforge.net>
2021-11-17 21:56:53 +0900

    support myproxy_password secret file.

--- nextcloud/config.sh
+++ nextcloud/config.sh
@@ -2,9 +2,9 @@
 
 NEXTCLOUD_USER=www-data
 SUDO_USER="sudo -s -u ${NEXTCLOUD_USER}"
-DATA_DIR=${NEXTCLOUD_DATA_DIR:-/var/www/html/data}
+DATA_DIR="${NEXTCLOUD_DATA_DIR:-/var/www/html/data}"
 TMP_DATA_DIR="${DATA_DIR}.bak"
-MNT_OPT="-o nonempty,modules=subdir,subdir=${GFARM_DATA_PATH},entry_timeout=${FUSE_ENTRY_TIMEOUT},negative_timeout=${FUSE_NEGATIVE_TIMEOUT},attr_timeout=${FUSE_ATTR_TIMEOUT}"
+MNT_OPT="-o nonempty,modules=subdir,subdir=${GFARM_DATA_PATH},entry_timeout=${FUSE_ENTRY_TIMEOUT},negative_timeout=${FUSE_NEGATIVE_TIMEOUT},attr_timeout=${FUSE_ATTR_TIMEOUT},auto_cache,big_writes"
 
 SYSTEM_DIR=html
 SYSTEM_ARCH=${SYSTEM_DIR}.tar.gz
@@ -13,8 +13,8 @@ DB_ARCH=${DB_FILE}.gz
 LOG_FILE=nextcloud.log
 LOG_ARCH=${LOG_FILE}.gz
 
-NEXTCLOUD_SPOOL_PATH=/var/spool/nextcloud
-INIT_FLAG_PATH=${NEXTCLOUD_SPOOL_PATH}/init
-VOLUME_REUSE_FLAG_PATH=${NEXTCLOUD_SPOOL_PATH}/reuse
-RESTORE_FLAG_PATH=${NEXTCLOUD_SPOOL_PATH}/restore
-POST_FLAG_PATH=${NEXTCLOUD_SPOOL_PATH}/post
+NEXTCLOUD_SPOOL_PATH="/var/spool/nextcloud"
+INIT_FLAG_PATH="${NEXTCLOUD_SPOOL_PATH}/init"
+VOLUME_REUSE_FLAG_PATH="${NEXTCLOUD_SPOOL_PATH}/reuse"
+RESTORE_FLAG_PATH="${NEXTCLOUD_SPOOL_PATH}/restore"
+POST_FLAG_PATH="${NEXTCLOUD_SPOOL_PATH}/post"
--- nextcloud/entrypoint0.sh
+++ nextcloud/entrypoint0.sh
@@ -3,7 +3,8 @@
 # -*- mode: shell-script; indent-tabs-mode: nil; sh-basic-offset: 4; -*-
 
 set -eu
-set -x
+set -o pipefail
+#set -x
 
 source /config.sh
 
@@ -27,8 +28,8 @@ export NEXTCLOUD_LOG_PATH=${NEXTCLOUD_LOG_PATH}
 export NEXTCLOUD_BACKUP_TIME="${NEXTCLOUD_BACKUP_TIME}"
 
 export GFARM_USER=${GFARM_USER}
-export GFARM_DATA_PATH=${GFARM_DATA_PATH}
-export GFARM_BACKUP_PATH=${GFARM_BACKUP_PATH}
+export GFARM_DATA_PATH="${GFARM_DATA_PATH}"
+export GFARM_BACKUP_PATH="${GFARM_BACKUP_PATH}"
 export GFARM_ATTR_CACHE_TIMEOUT=${GFARM_ATTR_CACHE_TIMEOUT}
 
 export TZ=${TZ}
@@ -68,12 +69,6 @@ GFARM_USERMAP="${HOMEDIR}/.gfarm_usermap"
 echo "${GFARM_USER} ${NEXTCLOUD_USER}" > "${GFARM_USERMAP}"
 chown0 "${GFARM_USERMAP}"
 
-GFARM_SHARED_KEY="${HOMEDIR}/.gfarm_shared_key"
-copy0 "/gfarm_shared_key" "${GFARM_SHARED_KEY}"
-
-GFARM2RC="${HOMEDIR}/.gfarm2rc"
-copy0 "/gfarm2rc" "${GFARM2RC}"
-
 GFARM_CONF="/usr/local/etc/gfarm2.conf"
 cp "/gfarm2.conf" "${GFARM_CONF}"
 echo >> "${GFARM_CONF}"
@@ -81,16 +76,34 @@ echo "local_user_map ${GFARM_USERMAP}" >> "${GFARM_CONF}"
 echo "attr_cache_timeout ${GFARM_ATTR_CACHE_TIMEOUT:-180}" >> "${GFARM_CONF}"
 chown0 "${GFARM_CONF}"
 
-DOT_GLOBUS="${HOMEDIR}/.globus"
-copy0 "/dot_globus" "${DOT_GLOBUS}"
+if [ -f "/gfarm2rc" ]; then
+    GFARM2RC="${HOMEDIR}/.gfarm2rc"
+    copy0 "/gfarm2rc" "${GFARM2RC}"
+fi
+
+if [ -f "/gfarm_shared_key" ]; then
+    GFARM_SHARED_KEY="${HOMEDIR}/.gfarm_shared_key"
+    copy0 "/gfarm_shared_key" "${GFARM_SHARED_KEY}"
+fi
+
+if [ -d "/dot_globus" ]; then
+    DOT_GLOBUS="${HOMEDIR}/.globus"
+    copy0 "/dot_globus" "${DOT_GLOBUS}"
+fi
 
 if [ -f "${GRID_PROXY_PASSWORD_FILE}" ]; then
     cat "${GRID_PROXY_PASSWORD_FILE}" | \
-        ${SUDO_USER} grid-proxy-init -pwstdin -hours ${GRID_PROXY_HOURS}
+        ${SUDO_USER} grid-proxy-init -pwstdin -hours "${GRID_PROXY_HOURS}"
     ${SUDO_USER} grid-proxy-info
 fi
 
-#TODO secret for myproxy password, MYPROXY_PASSWORD_FILE
+if [ -f "${MYPROXY_PASSWORD_FILE}" -a -n "${MYPROXY_SERVER}" ]; then
+    MYPROXY_USER=${MYPROXY_USER:-${GFARM_USER}}
+    cat "${MYPROXY_PASSWORD_FILE}" | \
+        ${SUDO_USER} myproxy-logon --stdin_pass \
+        -s "${MYPROXY_SERVER}" -l "${MYPROXY_USER}" -t "${GRID_PROXY_HOURS}"
+    ${SUDO_USER} grid-proxy-info
+fi
 
 if [ ! -f ${INIT_FLAG_PATH} ]; then
     sed -i -e 's/^NAME_COMPATIBILITY=STRICT_RFC2818$/NAME_COMPATIBILITY=HYBRID/' /etc/grid-security/gsi.conf
@@ -111,8 +124,8 @@ if [ $num_gfsd -le 0 ]; then
     exit 1
 fi
 
-${SUDO_USER} gfmkdir -p ${GFARM_DATA_PATH}
-${SUDO_USER} gfchmod 750 ${GFARM_DATA_PATH}
+${SUDO_USER} gfmkdir -p "${GFARM_DATA_PATH}"
+${SUDO_USER} gfchmod 750 "${GFARM_DATA_PATH}"
 
 MYSQL_PASSWORD="$(cat ${MYSQL_PASSWORD_FILE})"
 until mysqladmin ping -h ${MYSQL_HOST} -u ${MYSQL_USER} -p"${MYSQL_PASSWORD}"; do
@@ -122,7 +135,7 @@ done
 
 FILE_NUM=$(ls -1a --ignore=. --ignore=.. ${HOMEDIR}/html | wc -l)
 if [ ${FILE_NUM} -eq 0 ]; then
-    if ${SUDO_USER} gftest -d ${GFARM_BACKUP_PATH}; then
+    if ${SUDO_USER} gftest -d "${GFARM_BACKUP_PATH}"; then
         /restore.sh
     fi
 else
