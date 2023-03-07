94b230088512b8a1ddf15281afb9539371462b80
Takuya Ishibashi <takuya-i@users.sourceforge.net>
2023-03-08 01:41:54 +0900

    fix "ln: failed to create symbolic link"

--- nextcloud/entrypoint0.sh
+++ nextcloud/entrypoint0.sh
@@ -231,7 +231,7 @@ fi
 if [ ${IMPORT_CA_FROM_TLS_CERTIFICATES_DIR} -eq 1 ]; then
     for entry in $(ls -1 ${TLS_CERTIFICATES_DIR}/*.[0-9]); do
         bname=$(basename "$entry")
-        ln -s "$entry" /usr/local/share/ca-certificates/${bname}.crt
+        ln -fs "$entry" /usr/local/share/ca-certificates/${bname}.crt
     done
     update-ca-certificates
 fi
